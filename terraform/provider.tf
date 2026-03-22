24b8faf542ac52dbd250c6f4986f28ad04dc06b3
Ashok <ashok.kumar54@gmail.com>
2026-03-22 23:11:01 +0530

    saving jenkinsfile

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "google"{
     credentials = file("C:/Users/ashok/Downloads/my-project-1-486406-0396773d2982.json")
-    project_id = var.project_id
+    project = var.project_id
     region = var.region
     zone = var.zone
 }
